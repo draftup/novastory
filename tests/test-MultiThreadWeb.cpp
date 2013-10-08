809c334fbdb525422129df15ef6dff4097f207c6
Alexey Kasyanchuk <degitx@gmail.com>
2013-10-08 16:52:28 +0300

    webtest добавлен

--- tests/test-MultiThreadWeb.cpp
+++ tests/test-MultiThreadWeb.cpp
@@ -6,28 +6,68 @@ class Test_MultiThreadWeb: public QObject
 {
 	Q_OBJECT
 private slots:
+	void basewebTest();
 	void multithreadTest();
 private:
+	void mChecker();
+signals:
+	void webQuit();
 };
 
 void Test_MultiThreadWeb::multithreadTest()
+{
+	/*
+	novastory::WebServer w;
+
+	const int SERVERS = 8;
+	QTcpSocket htmlReader[SERVERS];
+	for(int i = 0; i < SERVERS; ++i)
+	{
+		connect(&htmlReader[i], &QTcpSocket::connected, [&htmlReader, i](){
+			qDebug() << "connect " << i;
+			htmlReader[i].write("GET /index.html HTTP/1.1\nHost: www.example.com");
+		});
+		connect(&htmlReader[i], &QTcpSocket::disconnected, [this, i](){
+			qDebug() << "disconnect " << i;
+			emit webQuit();
+		});
+		connect(&htmlReader[i], &QTcpSocket::readyRead, [&htmlReader, i](){
+			QByteArray html = htmlReader[i].readAll();
+			QVERIFY(html.indexOf("HTTP/1.0 200", Qt::CaseInsensitive) >= 0);
+			qDebug() << "readed " << i;
+			htmlReader[i].close();
+		});
+		htmlReader[i].connectToHost("127.0.0.1", 8008);
+
+		QEventLoop loop;
+		connect(this, SIGNAL(webQuit()), &loop, SLOT(quit()));
+		loop.exec();
+	}
+	*/
+}
+
+void Test_MultiThreadWeb::basewebTest()
 {
 	novastory::WebServer w;
 
 	QTcpSocket htmlReader;
 	connect(&htmlReader, &QTcpSocket::connected, [&htmlReader](){
-		qDebug() << "connected";
+		qDebug() << "connect";
+		htmlReader.write("GET /index.html HTTP/1.1\nHost: www.example.com");
 	});
-	connect(&htmlReader, &QTcpSocket::disconnected, [&htmlReader](){
-		qDebug() << "disconnected";
+	connect(&htmlReader, &QTcpSocket::disconnected, [this](){
+		qDebug() << "disconnect";
+		emit webQuit();
 	});
 	connect(&htmlReader, &QTcpSocket::readyRead, [&htmlReader](){
 		QByteArray html = htmlReader.readAll();
-		qDebug() << html;
+		QVERIFY(html.indexOf("HTTP/1.0 200", Qt::CaseInsensitive) >= 0);
+		htmlReader.close();
 	});
 	htmlReader.connectToHost("127.0.0.1", 8008);
 
 	QEventLoop loop;
+	connect(this, SIGNAL(webQuit()), &loop, SLOT(quit()));
 	loop.exec();
 }
 
