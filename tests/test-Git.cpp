ce4a90a5eba06e6633566fa69d2936c553cb33ec
Alexey Kasyanchuk <degitx@gmail.com>
2014-03-14 21:48:53 +0300

    git revision works under windows

---
+++ tests/test-Git.cpp
@@ -0,0 +1,39 @@
+#include <QtTest>
+#include "utils/globals.h"
+
+using namespace novastory;
+
+class Test_Git: public QObject
+{
+	Q_OBJECT
+private slots:
+	void initTestCase();
+	void init();
+	void cleanup();
+	void cleanupTestCase();
+private:
+};
+
+void Test_Git::initTestCase()
+{
+	QVERIFY(QString(GIT_REVISION).toInt() > 0);
+}
+
+void Test_Git::init()
+{
+	
+}
+
+void Test_Git::cleanup()
+{
+	
+}
+
+void Test_Git::cleanupTestCase()
+{
+
+}
+
+/********************** DECLARE_TEST LIST ****************************/
+QTEST_MAIN(Test_Git)
+#include "test-Git.moc"
