39b37a303a3966031fae9f7cd31483ca021c082f
Alexey Kasyanchuk <degitx@gmail.com>
2014-03-16 17:56:36 +0300

    new schema

--- dump/novastory.sql
+++ dump/novastory.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `texteditor`;
 CREATE TABLE `texteditor` (
   `userid` int(10) unsigned NOT NULL,
   `text` mediumtext,
-  KEY `userid` (`userid`)
+  UNIQUE KEY `userid` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -53,7 +53,7 @@ CREATE TABLE `users` (
   `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`userid`),
   KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +62,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'9e018658fec3fc6e7fd4a5e7317aec70','1f8b9be4b10381dc1f86e0d98f539490','degitx@gmail.com','2014-02-21 22:41:41'),(21,'995ddff75421a80c6b932292422ab336','281f38d79f9703a2b3e23772e71721b2','testmail@test.com','2014-03-11 13:57:07');
+INSERT INTO `users` VALUES (1,'9e018658fec3fc6e7fd4a5e7317aec70','1f8b9be4b10381dc1f86e0d98f539490','degitx@gmail.com','2014-02-21 22:41:41'),(21,'995ddff75421a80c6b932292422ab336','281f38d79f9703a2b3e23772e71721b2','testmail@test.com','2014-03-16 14:40:34');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +90,7 @@ CREATE TABLE `usersverify` (
 
 LOCK TABLES `usersverify` WRITE;
 /*!40000 ALTER TABLE `usersverify` DISABLE KEYS */;
-INSERT INTO `usersverify` VALUES ('f95323606523b28c8b61c3c3dc3b308f','b96a6e46234a7eb9386ea75217e9f52b','87c17d029c6fa715fa776c2b1c27c0ae','dsdasd@gmail.com','2014-03-11 13:57:08');
+INSERT INTO `usersverify` VALUES ('8455a5b2861cb1f598f8206440437717','625344bcec63233712ccc9ec7bb84db7','e6f2409eef996308b61b8b2dda69c913','dsdasd@gmail.com','2014-03-16 14:40:36');
 /*!40000 ALTER TABLE `usersverify` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-03-12 16:25:27
+-- Dump completed on 2014-03-16 17:55:57
