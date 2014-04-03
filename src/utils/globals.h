1c1403b01647a34c259279ffe7f80ac98085203a
Alexey Kasyanchuk <degitx@gmail.com>
2014-04-04 01:36:02 +0300

    Doxygen test

--- src/utils/globals.h
+++ src/utils/globals.h
@@ -11,12 +11,86 @@
 
 namespace novastory
 {
-
+/**
+ * @brief	Generate MD5 hashstring from given string
+ *
+ * @author	Alexey Kasyanchuk
+ * @date	04.04.2014
+ *
+ * @param	str	Input string for md5 generation
+ *
+ * @return	Generated md5 hash string
+ */
 QString md5(const QString& str);
+
+/**
+ * @brief	Generate SHA1 hashstring from given string
+ *
+ * @author	Alexey Kasyanchuk
+ * @date	04.04.2014
+ *
+ * @param	str	Input string for sha1 generation
+ *
+ * @return	Generated sha1 hash string
+ */
 QString sha1(const QString& str);
+
+/**
+ * @brief	Get current unixtime
+ *
+ * @author	Alexey Kasyanchuk
+ * @date	04.04.2014
+ *
+ * @return	Current unixtime.
+ */
 qint64 unixtime();
+
+/**
+ * @brief	Sends a mail to given email address
+ *
+ * @author	Alexey Kasyanchuk
+ * @date	04.04.2014
+ *
+ * @param	to			 	Email address for given mail
+ * @param	subject		 	Mail subject
+ * @param	textofmessage	Sending message
+ *
+ * @see sendAsyncMail()		
+ *
+ * @note This method work synchronously. Use @ref sendAsyncMail() for async calls.
+ */
 void sendMail(const QString& to, const QString& subject, const QString& message);
+
+/**
+ * @brief	Sends a mail to given email address
+ *
+ * @author	Alexey Kasyanchuk
+ * @date	04.04.2014
+ *
+ * @param	to			 	Email address for given mail
+ * @param	subject		 	Mail subject
+ * @param	message			Sending message
+ * 							
+ * @see sendMail()		
+ *
+ * @note This method work asynchronously. Use @ref sendMail() for sync calls.
+ * 		 
+ */
 void sendAsyncMail(const QString& to, const QString& subject, const QString& message);
+
+/**
+ * @fn	inline QByteArray htmlHeaderGen(const QString& mimetype, int size)
+ *
+ * @brief	Generate base html header for file
+ *
+ * @author	Alexey Kasyanchuk
+ * @date	04.04.2014
+ *
+ * @param	mimetype	The mimetype of file
+ * @param	size		The size of file
+ *
+ * @return	Html header. Apeend you body to it.
+ */
 
 inline QByteArray htmlHeaderGen(const QString& mimetype, int size)
 {
