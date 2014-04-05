3aa6446b646274321f370afb8ac028bc55e918ee
Alexey Kasyanchuk <degitx@gmail.com>
2014-04-05 11:39:47 +0300

    передалка регистрации под новые реалии

--- public/js/novastory.js
+++ public/js/novastory.js
@@ -14,9 +14,10 @@ $(document).ready(function ()
 
 	function doLogin()
 	{
-		var login = $('#login-email-field').val();
-		var pass = $('#login-password-field').val();
-		NovastoryApi.login(login, pass, function (data) {
+		var login = $('#loginmail').val();
+		var pass = $('#loginpass').val();
+		NovastoryApi.login(login, pass, function (data)
+		{
 			if (data.logined != null && data.logined)
 			{
 				alert("login successful");
@@ -26,126 +27,170 @@ $(document).ready(function ()
 			{
 				alert("login failed");
 			}
-		});
+		}
+		);
 	}
 
-	function doRegistration()
+	function proceedRegistration(email_adress, password_original, password_verify, challenge, response)
 	{
-		var email_adress = $('#registration-email-field').val();
-		var password_original = $('#registration-password-field').val();
-		var password_verify = $('#registration-password-verify-field').val();
-		
-		if(email_adress == null || password_original == null || password_verify == null)
+		NovastoryApi.register(email_adress, password_original, challenge, response, function (data)
 		{
-			alert("input error");
-			return;
-		}
-		
-		if(password_original != password_verify)
-		{
-			alert("input error");
-			return;
-		}
-			NovastoryApi.register(email_adress, password_original, Recaptcha.get_challenge(), Recaptcha.get_response(), function (data)
-			{
 
-				if (data.error == null || data.error)
-				{
-					alert("error");
-				}
-				else
-				{
-					Recaptcha.destroy();
-					alert("okay");
-				}
-			});
+			if (data.error == null || data.error)
+			{
+				alert("error on registration");
+			}
+			else
+			{
+				$('#registration-panel #registration-body').text("Congratulations with registration");
+				alert("okay");
+			}
+		}
+		);
 
 	}
 
-	$('#login-controls').click(function ()
+	function doRecaptcha()
+	{
+		var email_adress = $('#registration-panel #regmail').val();
+		var password_original = $('#registration-panel #regpass').val();
+		var password_verify = $('#registration-panel #confregpass').val();
+
+		if (email_adress == null || password_original == null || password_verify == null)
+		{
+			alert("input error");
+			return;
+		}
+
+		if (password_original != password_verify)
+		{
+			alert("input error");
+			return;
+		}
+
+		if(!$('#registration-panel #checkterms').prop('checked'))
+		{
+			alert("you must agree with terms");
+			return;
+		}
+		
+		var trueRegistrationBody =  $('#registration-panel #registration-body').clone();
+		
+		// init captcha
+		Recaptcha.create(
+			"6LfjsO4SAAAAADEhNeb51KeXPpVu_vtFQRC0w4iO",
+			"registration-body",
+		{
+			theme : "red",
+			callback : function ()
+			{
+				$('#registration-body').keyup(function (e)
+				{
+					if (e.keyCode == 13)
+					{
+						var challenge = Recaptcha.get_challenge();
+						var response = Recaptcha.get_response();
+						Recaptcha.destroy();
+						$(this).unbind("keyup");
+						$('#registration-panel #registration-body').replaceWith(trueRegistrationBody);
+						proceedRegistration(email_adress, password_original, password_verify, challenge, response);
+					}
+				}
+				);
+
+				Recaptcha.focus_response_field();
+			}
+		}
+		);
+	}
+
+	$('#modal-login').click(function ()
 	{
 		if (!$('#login-panel').exists())
 		{
-			$('#page-content').after("\
-																															<div id=\"login-panel\">\
-																																<div class=\"content\">\
-																																	<input type=\"text\" id=\"login-email-field\" placeholder=\"Логин\" />\
-																																	<input type=\"password\" id=\"login-password-field\" placeholder=\"Пароль\" />\
-																																	<div class=\"registration\">Регистрация</div>\
-																																	<div class=\"password-forgotten\">Я забыл(а) пароль</div>\
-																																</div>\
-																																<div class=\"hidder\"></div>\
-																															</div>\
-																															");
-
-			$('#login-email-field').keyup(function (e)
+			$('#login-space').load('/modal-login.html', null, function ()
 			{
-				if (e.keyCode == 13)
+				$('#loginmail').keyup(function (e)
+				{
+					if (e.keyCode == 13)
+					{
+						doLogin();
+					}
+				}
+				);
+
+				$('#loginpass').keyup(function (e)
+				{
+					if (e.keyCode == 13)
+					{
+						doLogin();
+					}
+				}
+				);
+
+				$('#loginbutt').click(function ()
 				{
 					doLogin();
 				}
-			}
-			);
-
-			$('#login-password-field').keyup(function (e)
-			{
-				if (e.keyCode == 13)
-				{
-					doLogin();
-				}
-			}
-			);
-
-			$('#login-panel .registration').click(function ()
-			{
-				$("#login-panel").hide();
-
-				$('#page-content').after("\
-																																				<div id=\"registration-panel\">\
-																																					<div class=\"content\">\
-																																						<input type=\"text\" id=\"registration-email-field\" placeholder=\"Почтовый адресс\" />\
-																																						<input type=\"password\" id=\"registration-password-field\" placeholder=\"Пароль\" />\
-																																						<input type=\"password\" id=\"registration-password-verify-field\" placeholder=\"Подтвердите пароль\" />\
-																																						<div id=\"captcha-place\"></div>\
-																																						<input type=\"checkbox\" name=\"registration-agreement\" value=\"agree\">я согласен(на) с условиями лицензионного соглашения\
-																																					</div>\
-																																					<div class=\"hidder\"></div>\
-																																				</div>\
-																																				");
-				// init captcha
-				Recaptcha.create(
-					"6LfjsO4SAAAAADEhNeb51KeXPpVu_vtFQRC0w4iO",
-					"captcha-place",
-				{
-					theme : "red",
-					callback : Recaptcha.focus_response_field
-				}
 				);
 
-				$('#registration-email-field').keyup(function (e)
+				$('#login-panel #newacc').click(function ()
 				{
-					if (e.keyCode == 13)
-					{
-						doRegistration();
-					}
-				}
-				);
+					$("#login-panel").hide();
 
-				$('#registration-password-field').keyup(function (e)
-				{
-					if (e.keyCode == 13)
+					if (!$('#registration-panel').exists())
 					{
-						doRegistration();
-					}
-				}
-				);
+						$('#register-space').load('/modal-register.html', null, function ()
+						{
 
-				$('#registration-password-verify-field').keyup(function (e)
-				{
-					if (e.keyCode == 13)
-					{
-						doRegistration();
+							$('#registration-panel #myacc').click(function ()
+							{
+								$('#registration-panel').hide();
+								$('#login-panel').show();
+							}
+							);
+
+							$('#registration-panel #regmail').keyup(function (e)
+							{
+								if (e.keyCode == 13)
+								{
+									doRecaptcha();
+								}
+							}
+							);
+
+							$('#registration-panel #regpass').keyup(function (e)
+							{
+								if (e.keyCode == 13)
+								{
+									doRecaptcha();
+								}
+							}
+							);
+
+							$('#registration-panel #confregpass').keyup(function (e)
+							{
+								if (e.keyCode == 13)
+								{
+									doRecaptcha();
+								}
+							}
+							);
+
+							$('#registration-panel #tocapcha').click(function ()
+							{
+								doRecaptcha();
+							}
+							);
+						}
+						);
 					}
+					else
+					{
+						$('#login-panel').hide();
+						$('#registration-panel').show();
+					}
+
 				}
 				);
 			}
@@ -160,6 +205,11 @@ $(document).ready(function ()
 			else
 			{
 				$("#login-panel").hide();
+			}
+
+			if (!$('#registration-panel').is(":hidden"))
+			{
+				$("#registration-panel").hide();
 			}
 		}
 	}
