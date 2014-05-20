50ccd2f595627f61184ea2db595bab88889b911d
Alexey Kasyanchuk <degitx@gmail.com>
2014-05-20 10:04:17 +0300

    неравильный тест для теста

--- tests/test-TextEditor.cpp
+++ tests/test-TextEditor.cpp
@@ -27,6 +27,7 @@ void Test_TextEditor::initTestCase()
 	buser.setRawPassword("doentcare");
 	QVERIFY(buser.addUser());
 	QVERIFY(buser.login("doentcare@dsadasd.ds", sha1("doentcare")));
+	QVERIFY(false);
 }
 
 void Test_TextEditor::init()
