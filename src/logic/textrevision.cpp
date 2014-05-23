e6f0a835075c325598136b02460a2af080089a98
Alexey Kasyanchuk <degitx@gmail.com>
2014-05-23 11:35:03 +0300

    астиль

--- src/logic/textrevision.cpp
+++ src/logic/textrevision.cpp
@@ -6,54 +6,54 @@
 namespace novastory
 {
 
-	int TextRevision::userid() const
-	{
-		return m_user.userid();
-	}
+int TextRevision::userid() const
+{
+	return m_user.userid();
+}
 
-	void TextRevision::setUserID(int userid)
-	{
-		m_user.setUserID(userid);
-	}
+void TextRevision::setUserID(int userid)
+{
+	m_user.setUserID(userid);
+}
 
-	const QString& TextRevision::text() const
-	{
-		return m_text;
-	}
+const QString& TextRevision::text() const
+{
+	return m_text;
+}
 
-	void TextRevision::setText(const QString& text)
-	{
-		m_text = text;
-	}
+void TextRevision::setText(const QString& text)
+{
+	m_text = text;
+}
 
-	void TextRevision::resetText()
-	{
-		m_text = QString();
-	}
+void TextRevision::resetText()
+{
+	m_text = QString();
+}
 
-	void TextRevision::setUser(const User& user)
-	{
-		m_user = user;
-	}
+void TextRevision::setUser(const User& user)
+{
+	m_user = user;
+}
 
-	const int& TextRevision::revision() const
-	{
-		return m_revision;
-	}
+const int& TextRevision::revision() const
+{
+	return m_revision;
+}
 
-	void TextRevision::setRevision( int revision )
-	{
-		m_revision = revision;
-	}
+void TextRevision::setRevision(int revision)
+{
+	m_revision = revision;
+}
 
-	const int& TextRevision::revisionId() const
-	{
-		return m_revisionId;
-	}
+const int& TextRevision::revisionId() const
+{
+	return m_revisionId;
+}
 
-	void TextRevision::setRevisionID( int revision )
-	{
-		m_revisionId = revision;
-	}
+void TextRevision::setRevisionID(int revision)
+{
+	m_revisionId = revision;
+}
 
 }
