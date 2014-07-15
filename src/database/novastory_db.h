6ae4c1b1f4893237daf2dcd38eddce6c3d821da6
Alexey Kasyanchuk <degitx@gmail.com>
2014-07-15 05:02:53 +0300

    астиль

--- src/database/novastory_db.h
+++ src/database/novastory_db.h
@@ -3,303 +3,311 @@
 
 #include "sql/dbpatcher.h"
 
-namespace novastory {
+namespace novastory
+{
 
 QSet<DBPatcher::Table> DB_TABLE_STRUCT()
 {
- QSet<DBPatcher::Table> TABLE_STRUCT;
- TABLE_STRUCT << DBPatcher::Table{
-  "avatars",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "userid",
-    "int(10) unsigned",
-    false,
-    "PRI",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "data",
-    "mediumblob",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "contenttype",
-    "varchar(64)",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "contentsize",
-    "int(10) unsigned",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "modifydate",
-    "timestamp",
-    false,
-    "",
-    "CURRENT_TIMESTAMP",
-    "on update CURRENT_TIMESTAMP"
-   },
-  })
-};
- TABLE_STRUCT << DBPatcher::Table{
-  "texteditor",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "userid",
-    "int(10) unsigned",
-    false,
-    "PRI",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "text",
-    "mediumtext",
-    true,
-    "",
-    "",
-    ""
-   },
-  })
-};
- TABLE_STRUCT << DBPatcher::Table{
-  "userspassforgot",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "token",
-    "char(32)",
-    false,
-    "PRI",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "userid",
-    "int(10) unsigned",
-    false,
-    "MUL",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "newpassword",
-    "char(32)",
-    false,
-    "",
-    "",
-    ""
-   },
-  })
-};
- TABLE_STRUCT << DBPatcher::Table{
-  "users",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "userid",
-    "int(10) unsigned",
-    false,
-    "PRI",
-    "",
-    "auto_increment"
-   },
-   DBPatcher::Column{
-    "password",
-    "char(32)",
-    false,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "salt",
-    "char(32)",
-    false,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "email",
-    "char(100)",
-    false,
-    "MUL",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "joindate",
-    "timestamp",
-    false,
-    "",
-    "CURRENT_TIMESTAMP",
-    "on update CURRENT_TIMESTAMP"
-   },
-   DBPatcher::Column{
-    "firstname",
-    "varchar(64)",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "lastname",
-    "varchar(64)",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "nickname",
-    "varchar(64)",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "profileid",
-    "varchar(64)",
-    true,
-    "MUL",
-    "",
-    ""
-   },
-  })
-};
- TABLE_STRUCT << DBPatcher::Table{
-  "subscriptions",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "userid",
-    "int(10) unsigned",
-    true,
-    "MUL",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "targetid",
-    "int(10) unsigned",
-    true,
-    "MUL",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "options",
-    "int(10) unsigned",
-    false,
-    "",
-    "0",
-    ""
-   },
-  })
-};
- TABLE_STRUCT << DBPatcher::Table{
-  "userspics",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "userid",
-    "int(10) unsigned",
-    false,
-    "PRI",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "data",
-    "mediumblob",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "contenttype",
-    "varchar(64)",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "contentsize",
-    "int(10) unsigned",
-    true,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "modifydate",
-    "timestamp",
-    false,
-    "",
-    "CURRENT_TIMESTAMP",
-    "on update CURRENT_TIMESTAMP"
-   },
-  })
-};
- TABLE_STRUCT << DBPatcher::Table{
-  "usersverify",
- QList<DBPatcher::Column>({
-   DBPatcher::Column{
-    "token",
-    "char(32)",
-    false,
-    "PRI",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "password",
-    "char(32)",
-    false,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "salt",
-    "char(32)",
-    false,
-    "",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "email",
-    "char(100)",
-    false,
-    "MUL",
-    "",
-    ""
-   },
-   DBPatcher::Column{
-    "date",
-    "timestamp",
-    false,
-    "",
-    "CURRENT_TIMESTAMP",
-    "on update CURRENT_TIMESTAMP"
-   },
-  })
-};
- return TABLE_STRUCT;
+	QSet<DBPatcher::Table> TABLE_STRUCT;
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"avatars",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"userid",
+				"int(10) unsigned",
+				false,
+				"PRI",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"data",
+				"mediumblob",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"contenttype",
+				"varchar(64)",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"contentsize",
+				"int(10) unsigned",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"modifydate",
+				"timestamp",
+				false,
+				"",
+				"CURRENT_TIMESTAMP",
+				"on update CURRENT_TIMESTAMP"
+			},
+		})
+	};
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"texteditor",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"userid",
+				"int(10) unsigned",
+				false,
+				"PRI",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"text",
+				"mediumtext",
+				true,
+				"",
+				"",
+				""
+			},
+		})
+	};
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"userspassforgot",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"token",
+				"char(32)",
+				false,
+				"PRI",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"userid",
+				"int(10) unsigned",
+				false,
+				"MUL",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"newpassword",
+				"char(32)",
+				false,
+				"",
+				"",
+				""
+			},
+		})
+	};
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"users",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"userid",
+				"int(10) unsigned",
+				false,
+				"PRI",
+				"",
+				"auto_increment"
+			},
+			DBPatcher::Column{
+				"password",
+				"char(32)",
+				false,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"salt",
+				"char(32)",
+				false,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"email",
+				"char(100)",
+				false,
+				"MUL",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"joindate",
+				"timestamp",
+				false,
+				"",
+				"CURRENT_TIMESTAMP",
+				"on update CURRENT_TIMESTAMP"
+			},
+			DBPatcher::Column{
+				"firstname",
+				"varchar(64)",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"lastname",
+				"varchar(64)",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"nickname",
+				"varchar(64)",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"profileid",
+				"varchar(64)",
+				true,
+				"MUL",
+				"",
+				""
+			},
+		})
+	};
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"subscriptions",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"userid",
+				"int(10) unsigned",
+				true,
+				"MUL",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"targetid",
+				"int(10) unsigned",
+				true,
+				"MUL",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"options",
+				"int(10) unsigned",
+				false,
+				"",
+				"0",
+				""
+			},
+		})
+	};
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"userspics",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"userid",
+				"int(10) unsigned",
+				false,
+				"PRI",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"data",
+				"mediumblob",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"contenttype",
+				"varchar(64)",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"contentsize",
+				"int(10) unsigned",
+				true,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"modifydate",
+				"timestamp",
+				false,
+				"",
+				"CURRENT_TIMESTAMP",
+				"on update CURRENT_TIMESTAMP"
+			},
+		})
+	};
+	TABLE_STRUCT << DBPatcher::Table
+	{
+		"usersverify",
+		QList<DBPatcher::Column>({
+			DBPatcher::Column{
+				"token",
+				"char(32)",
+				false,
+				"PRI",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"password",
+				"char(32)",
+				false,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"salt",
+				"char(32)",
+				false,
+				"",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"email",
+				"char(100)",
+				false,
+				"MUL",
+				"",
+				""
+			},
+			DBPatcher::Column{
+				"date",
+				"timestamp",
+				false,
+				"",
+				"CURRENT_TIMESTAMP",
+				"on update CURRENT_TIMESTAMP"
+			},
+		})
+	};
+	return TABLE_STRUCT;
 }
 
 }
