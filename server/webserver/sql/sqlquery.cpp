d926ecd3d5b09d2e8d864f01af19f8da87d2705b
Alexey Kasyanchuk <degitx@gmail.com>
2016-03-24 07:20:57 +0300

    Отображение времени запроса в логах

--- server/webserver/sql/sqlquery.cpp
+++ server/webserver/sql/sqlquery.cpp
@@ -3,16 +3,19 @@
 #include <QSqlError>
 #include <QThread>
 #include "sqldatabase.h"
+#include <QElapsedTimer>
 
 namespace novastory
 {
 
 bool SqlQuery::exec(const QString& query)
 {
+	QElapsedTimer timer;
+	timer.start();
 	bool result = QSqlQuery::exec(query);
 	if (result)
 	{
-		qDebug() << "SQL EXEC [OK]: " << query;
+		qDebug() << "SQL EXEC [OK]: " << query << "[time = " + QString::number((double)timer.nsecsElapsed() / 1000) + "qs]";
 	}
 	else
 	{
@@ -25,11 +28,13 @@ bool SqlQuery::exec(const QString& query)
 
 bool SqlQuery::exec()
 {
+	QElapsedTimer timer;
+	timer.start();
 	bool result = QSqlQuery::exec();
 	QList<QVariant> list = boundValues().values();
 	if (result)
 	{
-		qDebug() << "SQL EXEC [OK]: " << lastQuery();
+		qDebug() << "SQL EXEC [OK]: " << lastQuery() << "[time = " + QString::number((double)timer.nsecsElapsed() / 1000) + "qs]";
 		if (list.size() > 0)
 		{
 			qDebug() << "SQL Binded values:";
@@ -56,14 +61,17 @@ bool SqlQuery::exec()
 	return result;
 }
 
-SqlQuery::SqlQuery(const QString& query /*= QString()*/) : QSqlQuery(query, SqlDatabase::open(QThread::currentThreadId()))
+SqlQuery::SqlQuery(const QString& query /*= QString()*/)
 {
+	QElapsedTimer timer;
+	timer.start();
+	*this = QSqlQuery(query, SqlDatabase::open(QThread::currentThreadId()));
 	if (!query.isEmpty())
 	{
 		QSqlError error = lastError();
 		if (!error.isValid())
 		{
-			qDebug() << "SQL EXEC [OK]: " << lastQuery();
+			qDebug() << "SQL EXEC [OK]: " << lastQuery() << "[time = " + QString::number((double)timer.nsecsElapsed() / 1000) + "qs]";
 		}
 		else
 		{
